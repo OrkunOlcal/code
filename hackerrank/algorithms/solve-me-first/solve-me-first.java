a13f966f6af1adde763fce094c8c60c56c5144a0
OrkunOlcal <orkunolcal@gmail.com>
2022-06-16 16:25:40 +0200

    Add hackerrank algorithms solve me first in java and kotlin

---
+++ hackerrank/algorithms/solve-me-first/solve-me-first.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+        int sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+   
+}
